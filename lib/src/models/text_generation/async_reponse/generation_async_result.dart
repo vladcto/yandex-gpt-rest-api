6dc3a09ec77bae3c77c1b197be8e727cba010f3f
vladcto <55505918+vladcto@users.noreply.github.com>
2023-12-08 20:28:52 +0300

    feat: add model for TextGenerationAsync

---
+++ lib/src/models/text_generation/async_reponse/generation_async_result.dart
@@ -0,0 +1,29 @@
+sealed class GenerationAsyncResult {}
+
+final class ErrorAsyncResult extends GenerationAsyncResult {
+  final int code;
+  final String message;
+  final List<String> details;
+
+  ErrorAsyncResult({
+    required this.code,
+    required this.message,
+    required this.details,
+  });
+
+  factory ErrorAsyncResult.fromJson(Map<String, dynamic> json) {
+    return ErrorAsyncResult(
+      code: int.parse(json["code"] as String),
+      message: json["message"] as String,
+      details: List.of(json["details"] as List<dynamic>)
+          .map((i) => json["details"] as String)
+          .toList(),
+    );
+  }
+}
+
+final class ResponseAsyncResult extends GenerationAsyncResult {
+  final String body;
+
+  ResponseAsyncResult({required this.body});
+}
--- lib/src/models/text_generation/async_reponse/text_generation_async_response.dart
+++ lib/src/models/text_generation/async_reponse/text_generation_async_response.dart
@@ -1,1 +1,41 @@
-// TODO
+import 'package:yandex_gpt_rest_sdk/src/models/text_generation/async_reponse/generation_async_result.dart';
+
+class TextGenerationAsyncResponse {
+  final String id;
+  final String description;
+
+  // TODO: May i use DateTime?
+  final String createdAt;
+  final String createdBy;
+  final String modifiedAt;
+  final String metadata;
+  final GenerationAsyncResult? result;
+
+  bool get done => result != null;
+
+  TextGenerationAsyncResponse({
+    required this.id,
+    required this.description,
+    required this.createdAt,
+    required this.createdBy,
+    required this.modifiedAt,
+    required this.metadata,
+    required this.result,
+  });
+
+  factory TextGenerationAsyncResponse.fromJson(Map<String, dynamic> json) {
+    return TextGenerationAsyncResponse(
+      id: json["id"] as String,
+      description: json["description"] as String,
+      createdAt: json["createdAt"] as String,
+      createdBy: json["createdBy"] as String,
+      modifiedAt: json["modifiedAt"] as String,
+      metadata: json["metadata"] as String,
+      result: json["error"] != null
+          ? ErrorAsyncResult.fromJson(json["error"] as Map<String, dynamic>)
+          : json["response"] != null
+              ? ResponseAsyncResult(body: json["response"] as String)
+              : null,
+    );
+  }
+}
