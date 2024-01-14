d64aba59318a0ce7217d41ed6bae6c27d8ae655b
vladcto <55505918+vladcto@users.noreply.github.com>
2024-01-14 23:16:27 +0300

    Add docs for remaining models.

--- lib/src/models/embeddings/embedding_request.dart
+++ lib/src/models/embeddings/embedding_request.dart
@@ -2,6 +2,7 @@ import 'package:yandex_gpt_rest_api/src/models/gpt_models/v_model.dart';
 
 class EmbeddingRequest {
   final VModel model;
+  /// The input text for which the embedding is requested.
   final String text;
 
   const EmbeddingRequest({required this.model, required this.text});
--- lib/src/models/gpt_models/v_model.dart
+++ lib/src/models/gpt_models/v_model.dart
@@ -1,6 +1,9 @@
 import 'package:yandex_gpt_rest_api/src/models/gpt_models/gpt_model.dart';
 import 'package:yandex_gpt_rest_api/src/utils/constants/models_uri.dart';
 
+/// Models for vector representation of text.
+///
+/// Check [Yandex docs](https://cloud.yandex.ru/en/docs/yandexgpt/concepts/models#yandexgpt-embeddings) for more info.
 class VModel extends GptModel {
   const VModel.raw(String uri) : super.raw(uri: uri);
 
